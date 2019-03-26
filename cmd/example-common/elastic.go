ceef7a28e34445b74b62f08021ced5cd7817a26d
Gil Raphaelli <g@raphaelli.com>
2019-03-26 14:10:04 -0400

    enable elastic apm tracer logging

--- cmd/example-common/elastic.go
+++ cmd/example-common/elastic.go
@@ -2,6 +2,8 @@ package common
 
 import (
 	"io"
+	"log"
+	"os"
 
 	"github.com/opentracing/opentracing-go"
 	"go.elastic.co/apm"
@@ -21,8 +23,22 @@ func (f flushCloser) Close() error {
 	return nil
 }
 
+type elasticLogger struct {
+	*log.Logger
+}
+
+func (l *elasticLogger) Debugf(format string, args ...interface{}) {
+	l.Printf("[debug] "+format, args...)
+}
+
+func (l *elasticLogger) Errorf(format string, args ...interface{}) {
+	l.Printf("[error] "+format, args...)
+}
+
 func ElasticTracer() (opentracing.Tracer, io.Closer) {
 	elasticTracer := apm.DefaultTracer
+	logger := log.New(os.Stderr, "elastic ", log.Ldate|log.Ltime|log.Lshortfile)
+	apm.DefaultTracer.SetLogger(&elasticLogger{Logger: logger})
 	tracer := apmot.New(apmot.WithTracer(elasticTracer))
 	return tracer, flushCloser{flusher: elasticTracer}
 }
